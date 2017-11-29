f520f1c404423943ad808335c3b163c6b80a463d
B0B81 <schermer@gmx.net>
2017-11-29 21:02:12 +0100

    Updated Layout and Controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,6 @@
 class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /records
   # GET /records.json
--- app/controllers/workfiles_controller.rb
+++ app/controllers/workfiles_controller.rb
@@ -1,5 +1,6 @@
 class WorkfilesController < ApplicationController
   before_action :set_workfile, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /workfiles
   # GET /workfiles.json
